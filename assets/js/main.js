e78a00eb86833c2b3b7239dd01aaea3620b78433
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-10-05 15:52:27 +0200

    passwordInsicure js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,16 @@
 const userName = prompt(`Qual è il tuo nome?`)
+console.log(userName)
+
+
 const surName = prompt(`Qual è il tuo cognome?`)
+console.log(surName)
+
 const colorLike = prompt(`qual è il tuo colore preferito?`)
+console.log(colorLike)
+
+
+const passwordInsicure = userName + surName + colorLike
+ console.log(passwordInsicure)
+
+
+console.log(`Ciao la tua password isicurissima é: ${passwordInsicure}`)
