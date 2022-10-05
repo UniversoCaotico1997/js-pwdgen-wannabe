34493906d6ef5d9995df4f959a82fafea59eb18e
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-10-05 16:16:31 +0200

    Finish Insicurissimo password

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,21 @@
-const userName = prompt(`Qual è il tuo nome?`)
+const userName = prompt(`Qual è il tuo nome?`);
 console.log(userName)
 
 
-const surName = prompt(`Qual è il tuo cognome?`)
+const surName = prompt(`Qual è il tuo cognome?`);
 console.log(surName)
 
-const colorLike = prompt(`qual è il tuo colore preferito?`)
+const colorLike = prompt(`qual è il tuo colore preferito?`);
 console.log(colorLike)
 
+const number = 22;
+console.log(number)
 
-const passwordInsicure = userName + surName + colorLike
- console.log(passwordInsicure)
+
+const passwordInsicure = userName + surName + colorLike + number;
+ console.log(passwordInsicure);
 
 
-console.log(`Ciao la tua password isicurissima é: ${passwordInsicure}`)
+console.log(`Ciao la tua password isicurissima: ${passwordInsicure}`);
 
-document.getElementById("password").innerHTML = `Ciao la tua password isicurissima é: ${passwordInsicure}`
+document.getElementById("password").innerHTML = `Ciao la tua password isicurissima: ${passwordInsicure}`;
